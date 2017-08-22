cd50a98fe504543d0f3aeb1244ab64e62b2296f3
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-08-22 01:33:46 -0400

    Add conversion to int from dsInt and additional path element parsing

--- src/id.string.conversion.ts
+++ src/id.string.conversion.ts
@@ -1,6 +1,22 @@
 import { DatastoreInt } from '@google-cloud/datastore/entity';
+import * as debug from 'debug';
+
+const _DEBUG = debug('datastore-keyutil');
 
 export type DatastoreIdLike = string | number | DatastoreInt;
+
+export function isValidDsIntString(intstr: DatastoreIdLike): intstr is string {
+    intstr = intstr.toString();
+    return /^[1-9][\d]{0,15}$/.test(intstr);
+}
+
+export function isValidNumericId(id: number) {
+    return (id > 0) && (id < Number.MAX_SAFE_INTEGER);
+}
+
+export function isValidStringPathElement(e: DatastoreIdLike): e is string {
+    return (typeof e === 'string') && e.length > 0;
+}
 
 export function idToString(id: DatastoreIdLike): string {
     if (typeof id === 'number') {
@@ -15,10 +31,28 @@ export function idToString(id: DatastoreIdLike): string {
     return '';
 }
 
+export function dsIdToInt(datastoreId: DatastoreIdLike = ''): number {
+    if (typeof datastoreId === 'number' && Number.isSafeInteger(datastoreId)) {
+        return datastoreId;
+    }
+
+    datastoreId = dsIntToIntStr(datastoreId as DatastoreInt | string);
+
+    if (isValidDsIntString(datastoreId)) {
+        const result = parseInt(dsIntToIntStr(datastoreId));
+        if (isValidNumericId(result)) {
+            return result;
+        }
+    }
+
+    _DEBUG(`Invalid DS ID: ${datastoreId}`);
+    throw Error('key.invalidDsInt');
+}
+
 function isDatastoreInt_(id: any): id is DatastoreInt {
     return (id instanceof Object) && id.value;
 }
 
-export function isPositiveIntString(str: DatastoreIdLike): boolean {
-    return /^\d+$/.test(str.toString());
+function dsIntToIntStr(datastoreInt: DatastoreInt | string): string {
+    return (datastoreInt as DatastoreInt).value || datastoreInt as string;
 }
