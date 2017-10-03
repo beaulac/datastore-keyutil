553ba12df104480ce528881e52aafa0d87c8d3e8
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-10-03 01:29:18 -0400

    Restrict keylike interface more.

--- src/isKeylike.ts
+++ src/isKeylike.ts
@@ -1,8 +1,13 @@
 import { DatastoreKey } from '@google-cloud/datastore/entity';
 
-export interface DatastoreKeylike extends Pick<DatastoreKey, 'path'> {
+export interface DatastoreKeylike extends Pick<DatastoreKey, 'path' | 'kind' | 'id' | 'name'> {
+    parent?: DatastoreKeylike;
 }
 
 export function isKeylike(k: any): k is DatastoreKeylike {
-    return k && Array.isArray(k.path);
+    return k
+           && k.kind
+           && (k.id || k.name)
+           && k.path
+           && Array.isArray(k.path);
 }
